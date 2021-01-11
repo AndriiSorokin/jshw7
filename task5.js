3a4d3ed54c146960e249736ca5655e74534f5418
AndriiSorokin <68332371+AndriiSorokin@users.noreply.github.com>
2021-01-11 23:34:29 +0200

    сделал 5 таск

--- task5.js
+++ task5.js
@@ -1,7 +1,7 @@
 const input = document.querySelector('#name-input')
 const nameRef = document.querySelector('#name-output')
 
-input.addEventListener('input', function (event) {
+input.addEventListener('input', (event) => {
   if (event.target.value === '') {
     return (nameRef.textContent = 'незнакомец!')
   } else return (nameRef.textContent = event.target.value)
